963e4f57a2504280f0d6ea6ed76c03e1a8087491
Herman Morales Ara <hmoralesara@gmail.com>
2026-06-28 22:14:12 -0500

    subiendo cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,8 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     AuthModule,
     ConfigModule.forRoot(), // para acceder a variables de entorno
-    MongooseModule.forRoot(process.env.MONGO_URI!,{dbName:process.env.MONGO_DB_NAME}), //conexion bd mongo
+    //MongooseModule.forRoot(process.env.MONGO_URI!,{dbName:process.env.MONGO_DB_NAME}), //conexion bd mongo
+    MongooseModule.forRoot('mongodb://localhost:27017/test')
   ],
   controllers: [AppController],
   providers: [AppService],
